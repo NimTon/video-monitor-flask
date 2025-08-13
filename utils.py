e373ec94c6adde92ebc2bb940e6aa84b9263db17
NimTon <1350339182@qq.com>
2025-08-13 22:39:59 +0800

    修复bug

--- utils.py
+++ utils.py
@@ -120,10 +120,12 @@ def save_key_frames(stream_id, fence_id, frames, image_root='./images', base_url
     urls, paths = [], []
     for i, frame in enumerate([frames[0], frames[-1]], start=1):
         # 压缩成30KB以内
-        frame = compress_to_30kb(frame, max_size_kb=30)
+        img_bytes = compress_to_30kb(frame, max_size_kb=30)
         filename = f"{stream_id}_{fence_id}_{now_time_str}_{i}.jpg"
         filepath = os.path.join(image_root, filename)
-        cv2.imwrite(filepath, frame)
+        # cv2.imwrite(filepath, frame)
+        with open(filepath, "wb") as f:
+            f.write(img_bytes)
 
         file_url = f"{base_url}/images/{filename}"
         urls.append(file_url)
