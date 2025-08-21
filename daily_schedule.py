57c29ce481cd01a688194ba7c1e7a35d284b0204
Yang <1350339182@qq.com>
2025-08-21 14:13:20 +0800

    prompt传入相关信息

--- daily_schedule.py
+++ daily_schedule.py
@@ -134,7 +134,8 @@ class AutoReportScheduler:
             img_paths = [img.get("image_path") for img in images if img.get("image_path")]
             image_captions = [img.get("timestamp") for img in images if img.get("image_path")]
             camera_information = f"报告日期：{yesterday}，图片日期：{list(zip(img_paths, image_captions))}，stream_uid：{stream_uid}，stream_name：{stream_name}"
-            ai_summary = call_local_ai_model(ai_prompt=camera_information + daily_prompt, image_paths=img_paths, json_str=False)
+            daily_prompt = camera_information + daily_prompt
+            ai_summary = call_local_ai_model(ai_prompt=daily_prompt, image_paths=img_paths, json_str=False)
             # imgs_base64 = [image_path_to_base64(i) for i in img_paths]
             # ai_summary = call_qwen_via_client(daily_prompt, imgs_base64, model='qwen-vl-max-latest', json_str=False)
             # 更新当天 report 字段
@@ -170,7 +171,8 @@ class AutoReportScheduler:
         if individual_summaries:
             combined_prompt = daily_summary_prompt + "请基于以下各监控的AI总结生成一份总摘要:" + "\n".join(individual_summaries)
             today_information = f"报告日期：{yesterday}"
-            overall_summary = call_local_ai_model(ai_prompt=today_information + combined_prompt, json_str=False)
+            combined_prompt = today_information + combined_prompt
+            overall_summary = call_local_ai_model(ai_prompt=combined_prompt, json_str=False)
             # overall_summary = call_qwen_via_client(combined_prompt, model="qwen-plus", json_str=False)
             # 保存到特殊的总报告 UID，例如 "ALL_STREAMS"
             self.report_mgr.update_overall_summary(yesterday, overall_summary)
