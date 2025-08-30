7ea4cb084a3eaed04882a75ec35bebf6aaafe7f2
Yang <1350339182@qq.com>
2025-08-30 16:27:30 +0800

    初步构建异常检测模块

--- video_monitor/group_video_stream.py
+++ video_monitor/group_video_stream.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from db_utils import DBHelper
 from storage import StorageManager
 from video_monitor.fence_detector import FenceChangeDetector
-from utils import log
+from utils import log, resize_to_720p, draw_fence_on_frame
 
 storage_manger = StorageManager()
 detector = FenceChangeDetector()
@@ -13,10 +13,14 @@ db = DBHelper()
 
 # 激活的视频流列表
 streams = [stream for stream in storage_manger.list_streams() if stream.get("status") == "running"]
-
 # 队列用于模块间异步通信
 frame_queue = asyncio.Queue()
 detect_queue = asyncio.Queue()
+capture_path = "tmp/capture"
+detect_path = "tmp/detect"
+merge_path = "tmp/merge"
+
+change_threshold = 0.2
 
 
 # ------------------ 抓帧模块 ------------------
@@ -27,53 +31,60 @@ async def capture_stream(stream):
     url = stream.get("stream_url")
     fences = [f['id'] for f in stream.get("fences", [])]
 
-    os.makedirs("tmp", exist_ok=True)
+    os.makedirs(capture_path, exist_ok=True)
+    os.makedirs(detect_path, exist_ok=True)
+    os.makedirs(merge_path, exist_ok=True)
+
     cap = cv2.VideoCapture(url)
 
     while True:
         ret, frame = cap.read()
+        frame = resize_to_720p(frame)
         timestamp = datetime.now()
         if ret:
-            frame_path = f"tmp/capture/{stream_uid}_{timestamp.strftime('%Y%m%d_%H%M%S')}.jpg"
+            frame_path = f"{capture_path}/{stream_uid}_{timestamp.strftime('%Y%m%d_%H%M%S')}.jpg"
             success = cv2.imwrite(frame_path, frame)
             if success:
-                # 插入抓帧表
                 frame_id = db.insert_frame(stream_uid, group_uid, timestamp, frame_path)
-                log("INFO", f"抓取视频帧成功: {stream_name} ({stream_uid}), frame_id={frame_id}")
-
-                # 对每个围栏加入检测队列
+                # log("INFO", f"[抓帧] 抓取视频帧成功: {stream_name} ({stream_uid}), frame_id={frame_id}")
                 for fence_id in fences:
-                    await detect_queue.put((frame_id, fence_id, frame_path, timestamp))
+                    await detect_queue.put((stream_name, stream_uid, frame_id, group_uid, fence_id, frame_path, timestamp))
             else:
-                log("FAIL", f"抓取视频帧失败: {stream_name} ({stream_uid}), 保存路径={frame_path}")
+                log("FAIL", f"[抓帧] 抓取视频帧失败: {stream_name} ({stream_uid}), 保存路径={frame_path}")
         else:
-            log("WARNING", f"读取视频帧失败: {stream_name} ({stream_uid})")
+            log("WARNING", f"[抓帧] 读取视频帧失败: {stream_name} ({stream_uid})")
         await asyncio.sleep(1)
 
-# ------------------ 异常检测模块 ------------------
-async def detect_worker(detectors):
-    while True:
-        frame_id, fence_id, frame_path, timestamp = await detect_queue.get()
-        stream_uid = db.get_frame_stream(frame_id)  # 假设新增方法获取 stream_uid
-        detector = detectors.get(stream_uid)
-        if detector is None:
-            log("WARNING", f"[Detect] Stream {stream_uid} 无 detector，标记 normal")
-            db.insert_detection(stream_uid, group_uid, fence_id, 0, False, timestamp, frame_path)
-            detect_queue.task_done()
-            continue
 
+# ------------------ 异常检测模块 ------------------
+async def detect_worker():
+    while True:
+        stream_name, stream_uid, frame_id, group_uid, fence_id, frame_path, timestamp = await detect_queue.get()
         frame = cv2.imread(frame_path)
         if frame is None:
-            log("FAIL", f"[Detect] 读取帧失败: {frame_path}")
+            log("FAIL", f"[检测] {stream_name} (UID={stream_uid}, FRENCE_UID={fence_id}) 读取帧失败: {frame_path}")
             db.insert_detection(stream_uid, group_uid, fence_id, 0, False, timestamp, frame_path)
             detect_queue.task_done()
             continue
-
-        changed, area = detector.detect_change(frame, fence_id=fence_id, change_threshold=0.1)
-        db.insert_detection(stream_uid, group_uid, fence_id, change_ratio=area, changed=changed, timestamp=timestamp, frame_path=frame_path)
-        log("INFO", f"[Detect] {stream_uid}-{fence_id} 检测结果: {'abnormal' if changed else 'normal'}, area={area}")
-        detect_queue.task_done()
+        height, width = frame.shape[:2]
+        fence = storage_manger.get_fence(stream_uid, fence_id)
+        fence_points = []
+        points = fence.get('points', [])
+        if len(points) >= 3:
+            fence_points = [(int(p['x'] * width), int(p['y'] * height)) for p in points]
+        detector.set_fence(fence_points)
+        changed, change_area, change_ratio = detector.detect_change(frame)
+        change_ratio = round(change_ratio, 4)
+        db.insert_detection(stream_uid, group_uid, fence_id, change_ratio=change_ratio, changed=changed, timestamp=timestamp, frame_path=frame_path)
+        if change_threshold <= change_ratio <= 1:
+            frame_path = f"{detect_path}/{stream_uid}_{fence_id}_{timestamp.strftime('%Y%m%d_%H%M%S')}.jpg"
+            frame = draw_fence_on_frame(frame, fence_points)
+            cv2.imwrite(frame_path, frame)
+        if 0 <= change_ratio <= 1:
+            detect_queue.task_done()
+            log("INFO", f"[检测] {stream_name} (UID={stream_uid}, FRENCE_UID={fence_id}) 变化率：{change_ratio} 检测结果: {'异常' if changed else '正常'}")
         await asyncio.sleep(0)
+
 
 # ------------------ 合成视频模块 ------------------
 async def merge_worker():
@@ -105,11 +116,10 @@ async def merge_worker():
 
                 # 插入视频合成表
                 size = os.path.getsize(video_name)
-                duration = len(frame_paths)/5  # fps=5
+                duration = len(frame_paths) / 5  # fps=5
                 db.insert_merged_video(stream_uid, group_uid, fence_uid, video_name, duration, size, datetime.now())
-                log("INFO", f"[Merge] 合成视频完成: {video_name}, 帧数={len(frame_paths)}")
+                log("INFO", f"[合成] 合成视频完成: {video_name}, 帧数={len(frame_paths)}")
         await asyncio.sleep(10)
-
 
 
 # ------------------ 主程序 ------------------
@@ -117,23 +127,15 @@ async def main():
     # 每个视频流启动一个抓帧任务
     capture_tasks = [asyncio.create_task(capture_stream(stream)) for stream in streams]
 
-    # 初始化 detector
-    # detectors = {}
-    # for stream in streams:
-    #     stream_uid, fence_uid, group_uid, url, points = stream
-    #     det = FenceChangeDetector()
-    #     det.set_fence(points)
-    #     detectors[stream_uid] = det
-
     # 启动检测 worker
-    # detect_tasks = [asyncio.create_task(detect_worker(detectors)) for _ in range(3)]
+    detect_tasks = [asyncio.create_task(detect_worker()) for _ in range(3)]
 
     # 启动合成任务
     # merge_task = asyncio.create_task(merge_worker())
 
     # await asyncio.gather(*capture_tasks, *detect_tasks, merge_task)
 
-    await  asyncio.gather(*capture_tasks)
+    await  asyncio.gather(*capture_tasks, *detect_tasks)
 
 
 if __name__ == "__main__":
