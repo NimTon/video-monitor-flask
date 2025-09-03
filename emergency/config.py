e55b676c2663617f69558f8903c2b75a942309ce
NimTon <1350339182@qq.com>
2025-09-03 21:57:46 +0800

    添加api、prompt相关内容

--- emergency/config.py
+++ emergency/config.py
@@ -1,5 +1,10 @@
+URL_GET_DEVICES = "https://openapisit.zhongkaixingye.com/openapi/lot/ai/devices"
+URL_GET_LIVE_URL = "https://openapisit.zhongkaixingye.com/openapi/lot/ai/video/url"
+URL_UPLOAD_FILE = "https://openapisit.zhongkaixingye.com/openapi/lot/ai/file/upload"
+URL_EVENT_UP = "https://openapisit.zhongkaixingye.com/openapi/lot/ai/event/up"
 ZK_TOKEN = "FZK865AI9184C4A66"
 MACHINE_CODES = [
     "1",
     "2"
 ]
+PROMPT = ""
