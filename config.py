ef7d34698632e7d4a4cc4b195a96f362920a62fb
Yang <1350339182@qq.com>
2025-09-03 11:23:28 +0800

    新增引入环境变量

---
+++ config.py
@@ -0,0 +1,18 @@
+HOST = "127.0.0.1"
+PORT = 5000
+BASE_URL = f"{HOST}:{PORT}"
+
+ZLMK_SECRET = "RMys9486msj1NraRsncf0k0lpAMmLaHP"
+ZLMK_URL = "http://172.27.109.14/index/api"
+
+LOCAL_AI_IMAGES_URL = "http://10.30.4.32:5800/api/sayImages"
+LOCAL_AI_VIDEO_URL = "http://10.30.4.32:5800/api/sayVideo"
+LOCAL_AI_TEXT_URL = "http://10.30.4.32:5810/api/sayMsg"
+
+QWEN_API_KEY = "sk-f3ec150157ec41baaa516b15d1feaeae"
+QWEN_URL = "https://dashscope.aliyuncs.com/compatible-mode/v1"
+
+ZK_TOKEN = "FZK865AI9184C4A66"
+MACHINE_CODES = ["1", "2"]
+
+DB_PATH = "video_db.sqlite"
