717d25389b3444112dfb07dd4c50c9f3ccfa51f9
NimTon <1350339182@qq.com>
2025-09-19 21:09:58 +0800

    修改中凯api url

--- emergency/config.py
+++ emergency/config.py
@@ -1,11 +1,12 @@
-URL_GET_DEVICES = "https://openapisit.zhongkaixingye.com/openapi/lot/ai/devices"
-URL_GET_LIVE_URL = "https://openapisit.zhongkaixingye.com/openapi/lot/ai/video/url"
+URL_GET_DEVICES = "https://openapi.zhongkaixingye.com/openapi/lot/ai/devices"
+URL_GET_LIVE_URL = "https://openapi.zhongkaixingye.com/openapi/lot/ai/video/url"
 URL_UPLOAD_FILE = "https://openapisit.zhongkaixingye.com/openapi/lot/ai/file/upload"
 URL_EVENT_UP = "https://openapisit.zhongkaixingye.com/openapi/lot/ai/event/up"
-URL_QUERY_AND_PUSH_ASSETS = "https://your-domain.com/api/asset/device/queryAndPush"
+URL_QUERY_AND_PUSH_ASSETS = "http://10.30.3.150:9020/asset/assetDeviceRela/hj/queryRela"
 ZK_TOKEN = "FZK865AI9184C4A66"
 MACHINE_CODES = [
     "1",
     "2"
 ]
 PROMPT = ""
+API_KEY = "k8#Pm@3q!W9"
