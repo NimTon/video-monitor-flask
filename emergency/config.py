f116416daf23c1ca62a272f1ac4812ac53dcb053
user <1@1.com>
2025-09-22 15:16:51 +0800

    修改接口url

--- emergency/config.py
+++ emergency/config.py
@@ -2,7 +2,7 @@ URL_GET_DEVICES = "https://openapi.zhongkaixingye.com/openapi/lot/ai/devices"
 URL_GET_LIVE_URL = "https://openapi.zhongkaixingye.com/openapi/lot/ai/video/url"
 URL_UPLOAD_FILE = "https://openapisit.zhongkaixingye.com/openapi/lot/ai/file/upload"
 URL_EVENT_UP = "https://openapisit.zhongkaixingye.com/openapi/lot/ai/event/up"
-URL_QUERY_AND_PUSH_ASSETS = "http://10.30.3.150:9020/asset/assetDeviceRela/hj/queryRela"
+URL_QUERY_AND_PUSH_ASSETS = "http://103.25.65.102:9020/asset/assetDeviceRela/hj/queryRela"
 ZK_TOKEN = "FZK865AI9184C4A66"
 MACHINE_CODES = [
     "1",
