bd741d907facb1a1274ced4bc060fab98b369a95
user <1@1.com>
2025-10-24 22:27:36 +0800

    FLOW_BASE_URL  修改

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import json
+# FLOW_URL = "https://jrlyy.fusionfintrade.com:39100"  # 华为云转流服务
+FLOW_URL = "http://1.94.137.200:5001"   # 华为云转流服务
+BASE_URL = "http://1.94.137.200:5000" # 华为云基础服务
 
-FLOW_URL = "1.94.137.200:5001"  # 华为云转流服务
-BASE_URL = "1.94.137.200:5000" # 华为云基础服务
-# FLOW_URL = ""  # 公司转流服务
-# BASE_URL = "" # 公司基础服务
+# FLOW_URL = "http://10.30.3.178:5001"  # 公司转流服务
+# BASE_URL = "http://10.30.3.178:5000" # 公司基础服务
 
 LOCAL_AI_IMAGES_URL = "http://103.25.65.102:15800/api/sayImages"
 LOCAL_AI_VIDEO_URL = "http://103.25.65.102:15800/api/sayVideo"
